2c0fbd276492c66966475493f644ef34607fcc4d
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2022-10-21 02:20:26 +0300

    updated the run script

--- tests/run.sh
+++ tests/run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# remove previously compiled executable if exists
 if [ -f "test" ]; then
   rm "test"
 fi
 
+# compile a new executable
 gcc -Wall -Wextra -Werror -pedantic -std=gnu89 -Wno-format *.c -o test
-./test
+
+# run the new executable if the compilation was a success
+if [ -f "test" ]; then
+  ./test
+fi
