3f3cbc2d8633d8cafbb26c8b1a4fa5581c5bb8f7
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2022-10-25 17:24:36 +0300

    Update tests

--- tests/main.c
+++ tests/main.c
@@ -15,14 +15,12 @@ int main(void)
 	unsigned int ui;
 	void *addr;
 	char *tst;
-	char *rot;
 	int num;
 
 	num = 32978;
 	ui = (unsigned int) INT_MAX + 1024;
 	addr = (void *) 0x7ffe637541f0;
 	tst = "the quick brown fox jumps over the lazy dog";
-	rot = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
 
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
@@ -53,15 +51,16 @@ int main(void)
 	_printf("Address:[%p]\n", addr);
 	printf("Address:[%p]\n\n", addr);
 
-	len = _printf("Percent:[%%]\n");
-	len2 = printf("Percent:[%%]\n");
+	len = _printf("Percent:[%d%%]\n", 80);
+	len2 = printf("Percent:[%d%%]\n", 80);
 
 	_printf("\n");
 
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n\n", len2);
 
-	_printf("%S\n", "Best\nSchool");
+	_printf("%S\n", "Best\fSchool is\tthis\bone\ror\awhich\vone\fis\nit?");
+	printf("\n");
 	_printf("%i in binary is %b\n\n", num, num);
 
 	_printf("The value of num variable is %i and its address is %p\n", num, &num);
@@ -70,20 +69,44 @@ int main(void)
 	_printf("%d%% of %d == %d%% of %d\n", 80, 50, 50, 80);
 	_printf("The first letter in the alphabet is %c\n\n", 'A');
 
-	_printf("My country in reverse is: %r\n\n", "kenya");
+	_printf("%s in reverse is: %r\n\n", "strawberry", "strawberry");
 
-	_printf("%R\n\n", rot);
 
+	/* To make sure the string functions do not modify the initial string: */
 	printf("The initial string before reverse: %s.\n", tst);
-	_printf("The reverse is printed: %r\n", tst);
+	_printf("The reverse: %r\n", tst);
 	printf("The initial string after reverse: %s.\n\n", tst);
 
 	printf("The initial string before rot13: %s.\n", tst);
 	_printf("The rot13 of the string: %R.\n", tst);
 	printf("The initial string after rot13: %s.\n\n", tst);
 
-	/*_printf("Unknown:[%r]\n");
-	printf("Unknown:[%r]\n");*/
+	_printf("Unknown:[%k]\n");
+	printf("Unknown:[%k]\n\n");
+
+	_printf("Decimal: %d, Octal: %o, hex: %x, binary: %b\n\n", 195936478, 195936478, 195936478, 195936478);
+
+	len = printf("%d in HEX is %X\n", 195936478, 195936478);
+	len2 = _printf("%d in HEX is %X\n", 195936478, 195936478);
+
+	_printf("\n");
+
+	printf("Len: %i\n", len);
+	printf("Len: %i\n\n", len2);
+
+	/* Test the behavior of negative numbers */
+	printf("unsigned: %u\n", -num);
+	_printf("unsigned: %u\n\n", -num);
+
+	_printf("%u in hex is: %x\n", -429496724, -429496724);
+	printf("%u in hex is: %x\n\n", -429496724, -429496724);
+
+	printf("Long int: %u\n", ui);
+	_printf("Long int: %u\n\n", ui);
+
+	/* Testing for cases of zero: */
+	printf("%s in octal is %o\n", 0, 0);
+	_printf("%s in octal is %o\n", 0, 0);
 
 	return (0);
 }
